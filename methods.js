da052943598af7dc998e09eb846e1aa11aca178c
Jonas Almeida <jalmeida@mathbiol.org>
2022-08-31 21:47:31 -0400

    node communication

--- methods.js
+++ methods.js
@@ -14,6 +14,28 @@ async function insertPeerjs(){
     return msg
 }
 
+async function createNode(id=crypto.randomUUID()){ // this node's id, which peer nodes need to know to call in. If not provided a new one will be created
+    let node = new Peer(id)
+    return node
+}
 
+async function connect(node,id,msg=`sent at ${Date}`){ // connecting this node with peer node with given id
+    var conn = node.connect(id); 
+    // on open will be launch when you successfully connect to PeerServer
+    conn.on('open', function(){
+      // here you have conn.id
+      conn.send(msg);
+    });
+    return node
+}
 
-export {hello,insertPeerjs}
+async function receive(node){ // receiveing messages from those knowing my id
+    node.on('connection', function(conn) {
+      conn.on('data', function(data){
+        // Will print msg received
+        console.log(data);
+      });
+    });
+}
+
+export {hello,insertPeerjs,createNode}
